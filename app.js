e2cc69a4d19eeb8bd040ac30cb5db979d61ae853
JasonChou <swchou524@gmail.com>
2022-04-17 20:42:05 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
 // require packages used in the project
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
+// 引用路由器
+const routes = require('./routes')
+require('./config/mongoose')
+
 const app = express()
 
-// 載入環境變數
-const env = require('./env.json')
-
-// 載入 Restaurant model
-const Restaurant = require('./models/restaurant')
-
 const port = 3000
-mongoose.connect(env.MONGODB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
 
 // setting template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
@@ -30,19 +23,6 @@ app.use(methodOverride('_method'))
 // setting static files
 app.use(express.static('public'))
 
-//  取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
-// 引用路由器
-const routes = require('./routes')
 // 將 request 導入路由器
 app.use(routes)
 
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,21 +1,9 @@
-const mongoose = require('mongoose')
-const env = require('../../env.json')
-const Restaurant = require('../restaurant.js') // 載入 model
+const Restaurant = require('../restaurant') // 載入 model
 const restaurantList = require('../../restaurant.json')
-mongoose.connect(env.MONGODB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+
+const db = require('../../config/mongoose')
 
 db.once('open', () => {
-  console.log('mongodb connected! run the seeder')
   Restaurant.create(restaurantList.results)
   console.log('done')
 })
