101329c72b39be9d65e3f257a134eb7b550f404f
JasonChou <swchou524@gmail.com>
2022-04-17 11:50:11 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 // require packages used in the project
 const express = require('express')
+const mongoose = require('mongoose')
+const env = require('./env.json')
+
 const app = express()
 const port = 3000
+mongoose.connect(env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 
 // require express-handlebars here
 const exphbs = require('express-handlebars')
@@ -13,6 +20,17 @@ app.set('view engine', 'handlebars')
 
 // setting static files
 app.use(express.static('public'))
+
+//  取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // routes setting
 app.get('/', (req, res) => {
