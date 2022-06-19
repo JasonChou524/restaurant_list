cc339c840de9dc85c8a13902267f46fd556c88dd
JasonChou <swchou524@gmail.com>
2022-06-19 17:39:13 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const { redirect } = require('express/lib/response')
 const router = express.Router()
 
+// Login
 router.get('/login', (req, res) => {
   res.render('login')
+})
+
+router.post('/login', (req, res) => {
+  res.redirect('/')
+})
+
+// Register
+router.get('/register', (req, res) => {
+  res.render('register')
 })
 
 module.exports = router
